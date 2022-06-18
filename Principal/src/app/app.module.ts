4a195fadb2912f8a33a38017f9bb50dfa85d71ae
Meee3 <estudo.zzz@gmail.com>
2022-06-18 10:27:02 -0300

    Index

---
+++ Principal/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarrouselComponent } from './carrousel/carrousel.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarrouselComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
