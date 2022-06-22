8f0fd69ed0d985395b29dedd9e32eecfeebab119
evertondom <jony.everton-@hotmail.com>
2022-06-22 18:12:11 -0300

    mudanças no layout / adicionado login e cadastro

--- Principal/src/app/app-routing.module.ts
+++ Principal/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './telas/login/login.component';
+import { CadastroComponent } from './telas/cadastro/cadastro.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'home', pathMatch:'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'cadastro', component: CadastroComponent},
+  {path: 'login', component: LoginComponent}
+   
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Principal/src/app/app.module.ts
+++ Principal/src/app/app.module.ts
@@ -6,17 +6,31 @@ import { AppComponent } from './app.component';
 import { CarrouselComponent } from './carrousel/carrousel.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LocalizacaoComponent } from './localizacao/localizacao.component';
+import { CardComponent } from './card/card.component';
+import { CadastroComponent } from './telas/cadastro/cadastro.component';
+import { LoginComponent } from './telas/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarrouselComponent,
-    NavbarComponent
+    NavbarComponent,
+    LocalizacaoComponent,
+    CardComponent,
+    CadastroComponent,
+    LoginComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
