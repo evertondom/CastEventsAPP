9bdf532d654c736b7d61ad8e6f46832c978c0ef4
Unnamed <estudo.zzz@gmail.com>
2022-06-27 11:34:18 -0300

    Falta terminar a pagina ajuda

--- Principal/src/app/ajuda/ajuda.component.ts
+++ Principal/src/app/ajuda/ajuda.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AjudaComponent implements OnInit {
 
+  imagemHelp1 = 'https://www.pngmart.com/files/16/Help-PNG-Clipart.png';
+
   constructor() { }
 
   ngOnInit(): void {
